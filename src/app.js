6d7fb2d902a726b924cf04fa7ce61dab85788e5e
Vaibhav Singh Taragi <vaibhav.singh@finoptsys.com>
2025-06-08 11:25:51 +0530

    error-handling

--- src/app.js
+++ src/app.js
@@ -1,22 +1,37 @@
-const express = require('express');
-const {adminAuth, userAuth}= require('./middlewares/auth')
+const express = require("express");
+const { adminAuth, userAuth } = require("./middlewares/auth");
 
 const app = express();
 
-app.use('/admin',adminAuth)
+app.use("/admin", adminAuth);
 
-app.get('/admin/getAllData',(req,res)=>{
-    res.send('All data sent')
+app.get("/admin/getAllData", (req, res) => {
+  res.send("All data sent");
+});
+
+app.post("/user/login", (req, res) => {
+  res.send("User logged in");
+});
+
+// app.get("/user/getProfile", userAuth, (req, res) => {
+//   try {
+//     throw new error("errorrrrr");
+//     res.send("User Profile sent");
+//   } catch (err) {
+//     res.status(500).send("error found")
+//   }
+// });
+app.get("/user/getProfile",(req,res)=>{
+    throw new error('error')
+    res.send('User Profile Sent')
 })
 
-app.post('/user/login',(req,res)=>{
-    res.send('User logged in')
+app.use('/',(err,req,res,next)=>{
+    if(err){
+        res.status(500).send('error handled')
+    }
 })
 
-app.get('/user/getProfile',userAuth,(req,res)=>{
-    res.send('User Profile sent')
-})
-
-app.listen(4000,()=>{
-    console.log('server started')
-})
+app.listen(4000, () => {
+  console.log("server started");
+});
